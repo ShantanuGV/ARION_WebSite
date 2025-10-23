c2e14e67580baed398decf2855fcf81d5b02f73e
ShantanuGV <shantnuvispute@gmail.com>
2025-10-23 18:06:03 +0530

    Resolve merge conflict in About component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import React from "react";
 
 function About() {
   return (
     <section className="text-box" style={{ width: "100%", maxWidth: "1200px",marginTop: "8rem",marginBottom: "8rem" }}>
-=======
-import React from 'react'
-
-function About() {
-  return (
-    <section className="about">
->>>>>>> ab220416f93e8a52522b31372d7f1b6b619269b0
       <h2>About ARION</h2>
       <p>
         ARION is built to empower decision-makers by transforming complex market data into actionable insights.
@@ -22,13 +14,7 @@ function About() {
         the edge to act before volatility strikes.
       </p>
     </section>
-<<<<<<< HEAD
   );
 }
 
 export default About;
-=======
-  )
-}
-export default About
->>>>>>> ab220416f93e8a52522b31372d7f1b6b619269b0
